799d26dd1833341218bbac67fa4bf03fb482c6e0
kainatnawaz <52108680+kainatnawaz@users.noreply.github.com>
2023-02-26 23:26:16 +0500

    code updated

--- lib/widgets/PROFILE_BOTTOMSHEET.dart
+++ lib/widgets/PROFILE_BOTTOMSHEET.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-
 import '../view_Model/user_viewModel.dart';
 
 class ProfileScreen extends StatefulWidget {
